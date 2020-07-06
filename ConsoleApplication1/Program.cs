39df7df61ff584dafdd944f7cd578bb4ba295b96
Leon Cieslik <Leon.Cieslik@softwire.com>
2020-07-06 14:38:52 +0100

    Start of fizzbuzz

---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Security.AccessControl;
+
+namespace ConsoleApplication1
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int i;
+            for (i = 1; i < 101; i++)
+            {
+                var result = i.ToString();
+                if (i % 3 == 0)
+                {
+                    if (i % 5 == 0)
+                    {
+                        result = "FizzBuzz";
+                    }
+                    else
+                    {
+                        result = "Fizz";
+                    }
+                } 
+                Console.WriteLine(result);
+            }
+            
+        }
+    }
+}
